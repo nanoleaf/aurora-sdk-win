462114b72eb332c32181ad90e9fe1d8ecd418d0c
Lei Zhang <lei@nanoleaf.me>
2017-07-21 12:09:48 -0400

    release 2.0

--- Examples/SoundBar/Debug/src/subdir.mk
+++ Examples/SoundBar/Debug/src/subdir.mk
@@ -4,20 +4,22 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/AuroraPlugin.cpp 
+../src/AuroraPlugin.cpp \
+../src/AveragingFilter.cpp
 
 OBJS += \
-./src/AuroraPlugin.o 
+./src/AuroraPlugin.o \
+./src/AveragingFilter.o 
 
 CPP_DEPS += \
-./src/AuroraPlugin.d 
-
+./src/AuroraPlugin.d \
+./src/AveragingFilter.d
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -I../inc -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I../inc -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
